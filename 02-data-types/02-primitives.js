8d3f7e4a2fd56c9e6165cae94f86491413c12fc6
Sepehr Nikiyan <Sepehr.nik3@gmail.com>
2026-08-23 14:59:56 +0330

    adding typeof and primitives files to the project

---
+++ 02-data-types/02-primitives.js
@@ -0,0 +1,67 @@
+// string
+console.log(typeof "Sepehr");
+// number
+console.log(typeof 12);
+// boolean
+console.log(typeof true);
+// undefined
+console.log(typeof undefined);
+// null
+console.log(typeof null);
+
+//operation between strings and numbers
+console.log("5" * 2);
+console.log("20" / 4);
+console.log("10" + 10);
+console.log("10" - 10);
+console.log("Sepehr" - 5);
+
+
+//changing the types of the datas to number
+console.log(Number("10"));
+console.log(Number("Sepehr"));
+console.log(Number(""));
+console.log(Number(" "));
+console.log(Number("10.5"));
+
+
+//data type changing exercise
+console.log(String(123));
+console.log(String(true));
+console.log(Boolean(1));
+console.log(Boolean(0));
+console.log(Boolean(""));
+console.log(Boolean("Hello"));
+
+
+console.log(Boolean("0"));
+console.log(Boolean("false"));
+console.log(Boolean([]));
+console.log(Boolean({}));
+console.log(Boolean(null));
+console.log(Boolean(undefined));
+console.log(Boolean(NaN));
+console.log(Boolean(-1));
+
+
+//numbers operators
+console.log(10 + 5);
+console.log(10 - 5);
+console.log(10 * 5);
+console.log(10 / 5);
+console.log(10 % 3);
+console.log(2 ** 3);
+
+
+//exercises
+console.log(10 / 0);
+console.log(-10 / 0);
+console.log(0 / 0);
+console.log(typeof NaN);
+console.log(typeof Infinity);
+
+console.log(Number("100"));
+console.log(Number("100px"));
+console.log(parseInt("100px"));
+console.log(parseFloat("10.5px"));
+console.log(parseInt("10.5"));
