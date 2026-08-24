cf93440e499d5a7ad8f965dda7c6ca9b8c2e7b53
Sepehr Nikiyan <Sepehr.nik3@gmail.com>
2026-08-24 12:43:02 +0330

    practice data types and type conversion

--- 02-data-types/02-primitives.js
+++ 02-data-types/02-primitives.js
@@ -9,7 +9,7 @@ console.log(typeof undefined);
 // null
 console.log(typeof null);
 
-//operation between strings and numbers
+// Operations between strings and numbers
 console.log("5" * 2);
 console.log("20" / 4);
 console.log("10" + 10);
@@ -17,7 +17,7 @@ console.log("10" - 10);
 console.log("Sepehr" - 5);
 
 
-//changing the types of the datas to number
+// Converting values to Number
 console.log(Number("10"));
 console.log(Number("Sepehr"));
 console.log(Number(""));
@@ -25,7 +25,7 @@ console.log(Number(" "));
 console.log(Number("10.5"));
 
 
-//data type changing exercise
+// Type conversion exercises
 console.log(String(123));
 console.log(String(true));
 console.log(Boolean(1));
@@ -44,7 +44,7 @@ console.log(Boolean(NaN));
 console.log(Boolean(-1));
 
 
-//numbers operators
+// Arithmetic operators
 console.log(10 + 5);
 console.log(10 - 5);
 console.log(10 * 5);
@@ -65,3 +65,100 @@ console.log(Number("100px"));
 console.log(parseInt("100px"));
 console.log(parseFloat("10.5px"));
 console.log(parseInt("10.5"));
+
+
+//parseInt & parseFloat exercise
+console.log(parseInt("25.99"));
+console.log(parseFloat("25.99"));
+console.log(parseInt("100.50px"));
+console.log(parseFloat("100.50px"));
+console.log(parseInt("3.14abc"));
+console.log(parseFloat("3.14abc"));
+
+//Comparison Operators and Automatic Type Conversion
+console.log(5 == "5");
+console.log(5 === "5");
+console.log(5 != "5");
+console.log(5 !== "5");
+
+//exercises
+console.log(10 == "10");
+console.log(10 === "10");
+console.log(true == 1);
+console.log(true === 1);
+console.log(false == 0);
+console.log(false === 0);
+console.log(null == undefined);
+console.log(null === undefined);
+
+//Logical Operators
+console.log(true && true);
+console.log(true && false);
+console.log(false || true);
+console.log(false || false);
+console.log(!true);
+console.log(!false);
+
+
+// || returns the first Truthy value; if none exists, it returns the last value.
+// && returns the first Falsy value; if none exists, it returns the last value.
+console.log("Sepehr" && "Developer");
+console.log("" && "Developer");
+
+console.log("Sepehr" || "Developer");
+console.log("" || "Developer");
+
+console.log(null || "Default");
+
+console.log("Sepehr" && 25);
+
+
+// exercise
+console.log(0 || 100);
+console.log(50 || 100);
+
+console.log(0 && 100);
+console.log(50 && 100);
+
+console.log(null || "Guest");
+console.log("Sepehr" || "Guest");
+
+console.log("Hello" && 0 && "World");
+console.log("Hello" && 25 && "World");
+
+console.log("" || 0 || null || "Sepehr");
+console.log("" || 0 || null);
+
+console.log(!"Sepehr");
+console.log(!"");
+console.log(!0);
+console.log(!1);
+console.log(!null);
+console.log(!undefined);
+console.log(!!"Sepehr");
+console.log(!!0);
+
+//more exercise
+let a;
+
+const b = null;
+
+console.log(a);
+console.log(b);
+
+console.log(typeof a);
+console.log(typeof b);
+
+console.log(a == b);
+console.log(a === b);
+
+//another exercise
+let x;
+
+console.log(Boolean(x));
+console.log(Number(x));
+console.log(String(x));
+
+console.log(Boolean(null));
+console.log(Number(null));
+console.log(String(null));
